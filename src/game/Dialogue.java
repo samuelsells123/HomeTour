3d3ec56f92b4fab3ed00df5c7cb32ed53638ffe0
samuelsells123 <samuelsells123@gmail.com>
2021-05-27 20:35:03 -0500

    Initial Commit
    Completed a working implementation of a text adventure game that allows the player to move between multiple rooms.  Now, I need to refactor the Room class to use maps instead of arrays. I also started writing implementations for the Door, TrapDoor, Person, and Dialogue classes, though these are not implemented in the main class yet.

--- src/game/Dialogue.java
+++ src/game/Dialogue.java
@@ -4,6 +4,7 @@ public class Dialogue {
 	public String speaker;
 	public String sentence;
 	
+	// Class for storing dialogue for characters
 	public Dialogue(String speaker, String sentence) {
 		this.speaker = speaker;
 		this.sentence = sentence;
